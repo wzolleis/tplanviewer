9440c95f4acffe8ae326c1df5d076378c872cf2b
wzolleis <wzolleis@web.de>
2017-07-22 01:08:48 +0200

    Zugriff auf AWS mit Kotlin (einfacher Test)

---
+++ aws-client/src/main/kotlin/de/tpv/aws/client/App.kt
@@ -0,0 +1,33 @@
+package de.tpv.aws.client
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.regions.AwsEnvVarOverrideRegionProvider
+import com.sun.awt.SecurityWarning.getSize
+import com.amazonaws.services.s3.model.S3ObjectSummary
+import com.amazonaws.services.s3.model.ListObjectsV2Result
+import com.amazonaws.services.s3.model.ListObjectsV2Request
+
+
+fun main(args: Array<String>) {
+    val awsCreds = BasicAWSCredentials("AKIAJBFRNLDCXGMT6VEA", "AXeHsghXUAY72ZmKGPmY7h2wP1hOfhnxm96o9A+b")
+    var s3Client = AmazonS3ClientBuilder.standard()
+            .withCredentials(AWSStaticCredentialsProvider(awsCreds))
+            .withRegion("eu-central-1") //regionProvider.getRegion())
+            .build()
+
+    val buckets = s3Client.listBuckets();
+    for (bucket in buckets) {
+        println(bucket.name);
+    }
+
+    val listObjectsRequest = ListObjectsV2Request()
+    listObjectsRequest.bucketName = "de.wz.tpv"
+    val result = s3Client.listObjectsV2(listObjectsRequest)
+
+    for (summary in result.getObjectSummaries()) {
+        println(summary.getKey() + ":" + summary.getSize())
+    }
+
+}
---
+++ aws-client/src/main/kotlin/de/tpv/aws/client/s3/PlanRepo.kt
@@ -0,0 +1,11 @@
+package de.tpv.aws.client.s3
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.auth.BasicAWSCredentials
+
+
+
+class PlanRepo {
+
+}
